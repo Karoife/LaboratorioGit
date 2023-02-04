12f17f17524dc2530731cf5064ffdb53ed9fbd3a
Karoife <joserodolfo_coco@hotmail.com>
2023-02-04 12:32:38 -0600

    Se agrega un tercer gato

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -12,4 +12,12 @@ gato2.color = "cafe"
 gato2.edad = 3
 gato2.aniadirlista()
 
+
+gato3 = funcionesTairea.gato()
+gato3.nombre = "Minichu"
+gato3.color = "blanco"
+gato3.edad = 10
+gato3.aniadirlista()
+
+
 funcionesTarea.printlista()
